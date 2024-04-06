3fad72864da2dccd127c4c12d8a3ca9d876e7599
jysndabuio <jysndabu@outlook.com>
2024-04-06 09:48:35 +0200

    Solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!("Check if the string \"{string}\" contains balanced brackets");
+    let mut container = Vec::new();
+
+    for c in string.chars() {
+        if c == '(' || c == '[' || c == '{' {
+            container.push(c);
+        } else if c == ')' {
+            if container.pop() != Some('(') {
+                return false;
+            }
+        } else if c == ']' {
+            if container.pop() != Some('[') {
+                return false;
+            }
+        } else if c == '}' {
+            if container.pop() != Some('{') {
+                return false;
+            }
+        }
+    }
+
+    container.is_empty()
+
 }
